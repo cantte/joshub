e5756ce8aec7cd70ef447c9ed6cac99174d22630
Carlos Pavajeau <cantte098@gmail.com>
2022-12-03 19:15:01 -0500

    feat: add initial configuration
    * install all required packages
    * configure tailwind
    * add react query
    * add supabase
    * add and configure eslint and prettier
    * add .editorconfig

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps, AppType } from 'next/app'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient()
+
+const App: AppType = ({ Component, pageProps }: AppProps) => {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  return (
+    <SessionContextProvider supabaseClient={supabase}
+                            initialSession={pageProps.initialSession}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </SessionContextProvider>
+  )
 }
+
+export default App
