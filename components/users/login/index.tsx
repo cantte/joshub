e19cb4abba1a37b579a7d5ad5ce210385225a39e
Carlos Pavajeau <cantte098@gmail.com>
2022-12-06 14:14:15 -0500

    feat: rewrite login component

--- components/users/login/index.tsx
+++ components/users/login/index.tsx
@@ -1,27 +1,23 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
 
-const LoginPage: FC = () => {
+const Login: FC = () => {
   const session = useSession()
   const supabase = useSupabaseClient()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (session != null) {
+      void router.push('/').then(() => {
+      })
+    }
+  }, [session, router])
 
   return (
-    <div>
-      {(session == null)
-        ? (
-          <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>
-          )
-        : (
-          <div>
-            <h1>Logged in!</h1>
-            <button onClick={async () => await supabase.auth.signOut()}>Sign
-              out
-            </button>
-          </div>
-          )}
-    </div>
+    <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }}/>
   )
 }
 
-export default LoginPage
+export default Login
