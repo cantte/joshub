a5e2f6ef66793fdd8486b13ba01d9f0739f5d988
Carlos Pavajeau <cantte098@gmail.com>
2022-12-06 09:42:50 -0500

    feat(orders): add register form

--- components/shared/form/product.field.tsx
+++ components/shared/form/product.field.tsx
@@ -44,7 +44,7 @@ const ProductField: FC<Props> = ({ onSelected }) => {
           <Combobox.Input
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             onChange={(e) => setQuery(e.target.value)}
-            displayValue={(product: Product) => product === undefined ? 'Ningun producto seleccionado' : `${String(product.name)}, ${String(product.cold_spot_price)}`}
+            displayValue={(product: Product) => product === undefined ? '' : `${String(product.name)}, ${String(product.cold_spot_price)}`}
           />
         </div>
       </div>
