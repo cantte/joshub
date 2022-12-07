defb3d7115129907385ce0f86995b5b1f6d1f097
Carlos Pavajeau <cantte098@gmail.com>
2022-12-06 21:39:35 -0500

    feat: add new employees table

--- components/employees/table/index.tsx
+++ components/employees/table/index.tsx
@@ -3,7 +3,16 @@ import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { Employee } from '@joshub/types/employees'
 import { useQuery } from '@tanstack/react-query'
 import useCurrentEmployee from '@joshub/hooks/employees/use-current-employee'
-import { TrashIcon, PencilIcon } from '@heroicons/react/24/outline'
+import { PencilIcon, TrashIcon } from '@heroicons/react/24/outline'
+import {
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeaderCell,
+  TableRow
+} from '@tremor/react'
 
 const EmployeesTable: FC = () => {
   const supabase = useSupabaseClient()
@@ -20,63 +29,47 @@ const EmployeesTable: FC = () => {
 
   return (
     <div className="col-span-6">
-      <div className="overflow-x-auto relative">
-        <table
-          className="w-full text-sm text-left text-gray-500">
-          <thead
-            className="text-xs text-gray-700 uppercase bg-gray-50">
-          <tr>
-            <th scope="col" className="py-3 px-6">
-              Identificación
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Nombre
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Teléfono
-            </th>
-            <th scope="col" className="py-3 px-6">
-              Salario
-            </th>
-            <th scope="col" className="py-3 px-6"></th>
-          </tr>
-          </thead>
-          <tbody>
-          {
-            employees?.map(employee => (
-              <tr
-                key={employee.id}
-                className="bg-white border-b">
-                <th scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap">
-                  {employee.id}
-                </th>
-                <th scope="row">
-                  {employee.name} {currentEmployee?.id === employee.id ? '(Yo)' : ''}
-                </th>
-                <td className="py-4 px-6">
-                  {employee.phone}
-                </td>
-                <td className="py-4 px-6">
-                  ${employee.salary}
-                </td>
-                <td className="py-4 px-6">
-                  <button
-                    className="inline-flex justify-center rounded-full border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-50 focus:outline-none">
-                    <PencilIcon className="h-5 w-5 text-indigo-700"/>
-                  </button>
+      <Card>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableHeaderCell>Identificación</TableHeaderCell>
+              <TableHeaderCell>Nombre</TableHeaderCell>
+              <TableHeaderCell>Teléfono</TableHeaderCell>
+              <TableHeaderCell>Salario</TableHeaderCell>
+              <TableHeaderCell>Acciones</TableHeaderCell>
+            </TableRow>
+          </TableHead>
 
-                  <button
-                    className="inline-flex justify-center rounded-full border border-transparent bg-white px-4 py-2 text-sm font-medium text-red-700 hover:bg-red-50 focus:outline-none">
-                    <TrashIcon className="h-5 w-5 text-red-700"/>
-                  </button>
-                </td>
-              </tr>
-            ))
-          }
-          </tbody>
-        </table>
-      </div>
+          <TableBody>
+            {employees !== undefined && employees !== null
+              ? employees.map(employee => (
+                <TableRow key={employee.id}>
+                  <TableCell>{employee.id}</TableCell>
+                  <TableCell>{employee.name} {currentEmployee?.id === employee.id ? '(Yo)' : 'o'}</TableCell>
+                  <TableCell>{employee.phone}</TableCell>
+                  <TableCell>$ {Intl.NumberFormat('es').format(employee.salary)}</TableCell>
+                  <TableCell>
+                    <button
+                      className="inline-flex justify-center rounded-full border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-50 focus:outline-none">
+                      <PencilIcon className="h-5 w-5 text-indigo-700"/>
+                    </button>
+
+                    <button
+                      className="inline-flex justify-center rounded-full border border-transparent bg-white px-4 py-2 text-sm font-medium text-red-700 hover:bg-red-50 focus:outline-none">
+                      <TrashIcon className="h-5 w-5 text-red-700"/>
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))
+              : <TableRow>
+                <TableCell>
+                  No hay empleados
+                </TableCell>
+              </TableRow>}
+          </TableBody>
+        </Table>
+      </Card>
     </div>
   )
 }
