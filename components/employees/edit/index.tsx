cfb296728269218e52f813126dd1b86378b3e361
Carlos Pavajeau <cantte098@gmail.com>
2022-12-08 17:05:32 -0500

    feat(employees): add edit form

---
+++ components/employees/edit/index.tsx
@@ -0,0 +1,112 @@
+import { Employee } from '@joshub/types/employees'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import React, { FC } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useMutation } from '@tanstack/react-query'
+
+interface Props {
+  onUpdated: () => void
+
+  employee: Employee
+}
+
+const EditEmployeeForm: FC<Props> = ({ onUpdated, employee }) => {
+  const { register, handleSubmit, formState: { errors } } = useForm<Employee>({
+    defaultValues: employee
+  })
+  const supabase = useSupabaseClient()
+
+  const updateEmployee = async (data: Employee): Promise<void> => {
+    await supabase.from('employees').update(data).eq('id', data.id)
+  }
+
+  const {
+    mutate,
+    isLoading,
+    error
+  } = useMutation(updateEmployee, { onSuccess: onUpdated })
+
+  const onSubmit: SubmitHandler<Employee> = (data: Employee) => {
+    mutate(data)
+  }
+
+  return (
+    <div className="mt-5">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="sm:rounded-md">
+          <div className="bg-white px-4 py-5 pb-0">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="id"
+                       className="block text-sm font-medium text-gray-700">
+                  Cédula
+                </label>
+                <input type="text"
+                       disabled
+                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                       id="id" {...register('id', { required: true })} />
+
+                {(errors.id != null) &&
+                  <span className="text-red-400 text-xs block py-1">Este campo es requerido</span>}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="name"
+                       className="block text-sm font-medium text-gray-700">
+                  Nombre
+                </label>
+                <input type="text"
+                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                       id="name" {...register('name', { required: true })} />
+
+                {(errors.name != null) &&
+                  <span className="text-red-400 text-xs block py-1">Este campo es requerido</span>}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="phone"
+                       className="block text-sm font-medium text-gray-700">
+                  Teléfono
+                </label>
+                <input type="text"
+                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                       id="phone" {...register('phone', { required: true })} />
+
+                {(errors.phone != null) &&
+                  <span className="text-red-400 text-xs block py-1">Este campo es requerido</span>}
+              </div>
+
+              <div className="col-span-6 sm:col-span-3">
+                <label htmlFor="salary"
+                       className="block text-sm font-medium text-gray-700">
+                  Salario
+                </label>
+                <input type="number"
+                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                       id="salary" {...register('salary', { required: true })} />
+
+                {(errors.salary != null) &&
+                  <span className="text-red-400 text-xs block py-1">Este campo es requerido</span>}
+              </div>
+
+              <div className="py-3">
+                <button type="submit"
+                        disabled={isLoading}
+                        className="inline-flex justify-center rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2">
+                  Editar
+                </button>
+              </div>
+
+              {(Boolean(error)) &&
+                <div className="text-red-400 text-xs block py-1">
+                  Error al editar el empleado
+                </div>}
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default EditEmployeeForm
--- components/employees/table/index.tsx
+++ components/employees/table/index.tsx
@@ -14,6 +14,8 @@ import {
   TableRow
 } from '@tremor/react'
 import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/20/solid'
+import EditEmployeeForm from '@components/employees/edit'
 
 const EmployeesTable: FC = () => {
   const supabase = useSupabaseClient()
@@ -43,6 +45,15 @@ const EmployeesTable: FC = () => {
 
   const { mutate } = useMutation(deleteEmployee, { onSuccess: closeDeleteModal })
 
+  const [isOpeningEditModal, setIsOpeningEditModal] = useState(false)
+  const openEditModal = (): void => setIsOpeningEditModal(true)
+  const closeEditModal = (): void => {
+    setIsOpeningEditModal(false)
+    void queryClient.refetchQueries(['employees'])
+  }
+
+  const [employeeToEdit, setEmployeeToEdit] = useState<Employee | null>(null)
+
   return (
     <div className="col-span-6">
       <Card>
@@ -67,6 +78,10 @@ const EmployeesTable: FC = () => {
                   <TableCell>$ {Intl.NumberFormat('es').format(employee.salary)}</TableCell>
                   <TableCell>
                     <button
+                      onClick={() => {
+                        setEmployeeToEdit(employee)
+                        openEditModal()
+                      }}
                       className="inline-flex justify-center rounded-full border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-50 focus:outline-none">
                       <PencilIcon className="h-5 w-5 text-indigo-700"/>
                     </button>
@@ -164,6 +179,68 @@ const EmployeesTable: FC = () => {
                     >
                       No, cancelar
                     </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+
+      <Transition appear show={isOpeningEditModal} as={Fragment}>
+        <Dialog onClose={closeEditModal} as="div" className="relative z-10">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25"/>
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div
+              className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel
+                  className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    <div className="flex flex-col mb-5">
+                      <div className="flex flex-row justify-between">
+                        <h3
+                          className="text-xl font-semibold text-gray-900">
+                          Editar empleado
+                        </h3>
+                        <button
+                          onClick={() => setIsOpeningEditModal(false)}
+                          className="inline-flex justify-center rounded-full border border-transparent bg-white px-4 py-2 text-sm font-medium text-red-700 hover:bg-red-50 focus:outline-none">
+                          <XMarkIcon
+                            className="h-5 w-5 text-red-700"
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  </Dialog.Title>
+
+                  <div className="mt-2">
+                    {employeeToEdit !== null &&
+                      <EditEmployeeForm onUpdated={closeEditModal}
+                                        employee={employeeToEdit}/>
+                    }
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
