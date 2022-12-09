4a31c8bf565654b63bccc4bf61a3d9e5484d2735
Carlos Pavajeau <cantte098@gmail.com>
2022-12-09 10:15:20 -0500

    feat: new sales form ui
    move item addition view to a modal

--- components/sales/form/detail/index.tsx
+++ components/sales/form/detail/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import { FC } from 'react'
 import { Product } from '@joshub/types/products'
 import ProductField from '@components/shared/form/product.field'
 import { useForm } from 'react-hook-form'
@@ -14,14 +14,14 @@ const SaleDetailForm: FC<Props> = ({ onSubmit }) => {
   const {
     register,
     setValue,
-    getValues,
-    resetField,
     watch,
-    formState: { isValid, errors }
+    formState: { errors },
+    handleSubmit
   } = useForm<SaleDetailInput>({
     defaultValues: {
       product: undefined
-    }
+    },
+    reValidateMode: 'onChange'
   })
 
   const handleSelectProduct = (product: Product): void => {
@@ -29,173 +29,178 @@ const SaleDetailForm: FC<Props> = ({ onSubmit }) => {
     setValue('price', product.cold_spot_price)
   }
 
-  const [price, setPrice] = useState(0)
-  const handleOnSubmit = (): void => {
-    if (isValid) {
-      const values = getValues()
-      onSubmit({
-        ...values,
-        price
-      })
-
-      resetField('quantity')
-    }
-  }
-
   return (
     <div className="mt-5">
-      <div className="shadow sm:rounded-md">
-        <div className="bg-white px-4 py-5 sm:p-6">
-          <div className="grid grid-cols-6 gap-6">
-            <div className="col-span-6 sm:col-span-3">
-              <input
-                type="hidden" {...register('product', { required: true })} />
-              <input type="hidden" {...register('price', {
-                required: true,
-                max: watch('product.cold_spot_price')
-              })} />
-              <ProductField
-                onSelected={product => handleSelectProduct(product)}/>
-            </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="sm:rounded-md">
+          <div className="bg-white px-4">
+            <div className="grid grid-cols-6 gap-6">
+              <div className="col-span-6">
+                <input
+                  type="hidden" {...register('product', { required: true })} />
+                <input type="hidden" {...register('price', {
+                  required: true
+                })} />
+                <ProductField
+                  onSelected={product => handleSelectProduct(product)}/>
+              </div>
 
-            <div className="col-span-6 sm:col-span-3">
-              <label htmlFor="quantity"
-                     className="block text-sm font-medium text-gray-700">
-                Cantidad
-              </label>
-              <input type="number"
-                     className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                     id="quantity" {...register('quantity', {
-                       required: true,
-                       max: watch('product.quantity'),
-                       pattern: /^[0-9]+$/i
-                     })}/>
+              <div className="col-span-6">
+                <label htmlFor="quantity"
+                       className="block text-sm font-medium text-gray-700">
+                  Cantidad
+                </label>
+                <input type="number"
+                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                       id="quantity" {...register('quantity', {
+                         required: {
+                           value: true,
+                           message: 'La cantidad es requerida'
+                         },
+                         max: {
+                           value: watch('product.quantity'),
+                           message: 'La cantidad no puede ser mayor a la existencia'
+                         },
+                         min: {
+                           value: 1,
+                           message: 'La cantidad no puede ser menor a 1'
+                         },
+                         pattern: {
+                           value: /^[0-9]+$/i,
+                           message: 'Solo se permiten números'
+                         }
+                       })}/>
 
-              {watch('product.quantity') !== undefined && errors.quantity !== undefined &&
-                <span className="text-gray-400 text-xs block py-1">Cantidad máxima: {watch('product.quantity')}</span>}
+                {watch('product.quantity') !== undefined && errors.quantity?.message === undefined &&
+                  <span className="text-gray-400 text-xs block py-1">Cantidad máxima: {watch('product.quantity')}</span>}
 
-              {errors.quantity?.type === 'pattern' &&
-                <span className="text-red-500 text-xs block py-1">La cantidad debe ser un número entero</span>
-              }
-            </div>
+                {errors.quantity?.message !== undefined &&
+                  <span className="text-red-500 text-xs block py-1">
+                    {errors.quantity.message}
+                  </span>
+                }
+              </div>
 
-            {Boolean(watch('product')) && (
-              <div className="col-span-6 sm:col-span-3">
-                <RadioGroup value={price} onChange={setPrice}>
-                  <RadioGroup.Label
-                    className="sr-only">Precio</RadioGroup.Label>
-                  <div className="space-y-2">
-                    <RadioGroup.Option value={watch('product.cold_spot_price')}
-                                       className={({ active, checked }) =>
-                                         `${
-                                           active
-                                             ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
-                                             : ''
-                                         }
+              {Boolean(watch('product')) && (
+                <div className="col-span-6 sm:col-span-6">
+                  <RadioGroup value={watch('price')}
+                              onChange={e => setValue('price', e)}>
+                    <RadioGroup.Label
+                      className="sr-only">Precio</RadioGroup.Label>
+                    <div className="space-y-2">
+                      <RadioGroup.Option
+                        value={watch('product.cold_spot_price')}
+                        className={({ active, checked }) =>
+                          `${
+                            active
+                              ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
+                              : ''
+                          }
                   ${
-                                           checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'
-                                         }
+                            checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'
+                          }
                     relative flex cursor-pointer rounded-lg px-3 py-2 shadow-md focus:outline-none`
-                                       }
-                    >
-                      {({ checked }) => (
-                        <>
-                          <div
-                            className="flex w-full items-center justify-between">
-                            <div className="flex items-center">
-                              <div className="text-sm">
-                                <RadioGroup.Label
-                                  as="p"
-                                  className={`font-medium  ${
-                                    checked ? 'text-white' : 'text-gray-900'
-                                  }`}
-                                >
-                                  Precio punto frio
-                                </RadioGroup.Label>
-                                <RadioGroup.Description
-                                  as="span"
-                                  className={`inline ${
-                                    checked ? 'text-sky-100' : 'text-gray-500'
-                                  }`}
-                                >
+                        }
+                      >
+                        {({ checked }) => (
+                          <>
+                            <div
+                              className="flex w-full items-center justify-between">
+                              <div className="flex items-center">
+                                <div className="text-sm">
+                                  <RadioGroup.Label
+                                    as="p"
+                                    className={`font-medium  ${
+                                      checked ? 'text-white' : 'text-gray-900'
+                                    }`}
+                                  >
+                                    Precio punto frio
+                                  </RadioGroup.Label>
+                                  <RadioGroup.Description
+                                    as="span"
+                                    className={`inline ${
+                                      checked ? 'text-sky-100' : 'text-gray-500'
+                                    }`}
+                                  >
                             <span>
                               ${Intl.NumberFormat('es').format(watch('product.cold_spot_price'))}
                             </span>
-                                </RadioGroup.Description>
+                                  </RadioGroup.Description>
+                                </div>
                               </div>
+                              {checked && (
+                                <div className="shrink-0 text-white">
+                                  <CheckIcon className="h-6 w-6"/>
+                                </div>
+                              )}
                             </div>
-                            {checked && (
-                              <div className="shrink-0 text-white">
-                                <CheckIcon className="h-6 w-6"/>
-                              </div>
-                            )}
-                          </div>
-                        </>
-                      )}
-                    </RadioGroup.Option>
+                          </>
+                        )}
+                      </RadioGroup.Option>
 
-                    <RadioGroup.Option value={watch('product.watertight_price')}
-                                       className={({ active, checked }) =>
-                                         `${
-                                           active
-                                             ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
-                                             : ''
-                                         }
+                      <RadioGroup.Option
+                        value={watch('product.watertight_price')}
+                        className={({ active, checked }) =>
+                          `${
+                            active
+                              ? 'ring-2 ring-white ring-opacity-60 ring-offset-2 ring-offset-sky-300'
+                              : ''
+                          }
                   ${
-                                           checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'
-                                         }
+                            checked ? 'bg-sky-900 bg-opacity-75 text-white' : 'bg-white'
+                          }
                     relative flex cursor-pointer rounded-lg px-3 py-2 shadow-md focus:outline-none`
-                                       }
-                    >
-                      {({ checked }) => (
-                        <>
-                          <div
-                            className="flex w-full items-center justify-between">
-                            <div className="flex items-center">
-                              <div className="text-sm">
-                                <RadioGroup.Label
-                                  as="p"
-                                  className={`font-medium  ${
-                                    checked ? 'text-white' : 'text-gray-900'
-                                  }`}
-                                >
-                                  Precio estanco
-                                </RadioGroup.Label>
-                                <RadioGroup.Description
-                                  as="span"
-                                  className={`inline ${
-                                    checked ? 'text-sky-100' : 'text-gray-500'
-                                  }`}
-                                >
+                        }
+                      >
+                        {({ checked }) => (
+                          <>
+                            <div
+                              className="flex w-full items-center justify-between">
+                              <div className="flex items-center">
+                                <div className="text-sm">
+                                  <RadioGroup.Label
+                                    as="p"
+                                    className={`font-medium  ${
+                                      checked ? 'text-white' : 'text-gray-900'
+                                    }`}
+                                  >
+                                    Precio estanco
+                                  </RadioGroup.Label>
+                                  <RadioGroup.Description
+                                    as="span"
+                                    className={`inline ${
+                                      checked ? 'text-sky-100' : 'text-gray-500'
+                                    }`}
+                                  >
                             <span>
                               ${Intl.NumberFormat('es').format(watch('product.watertight_price'))}
                             </span>
-                                </RadioGroup.Description>
+                                  </RadioGroup.Description>
+                                </div>
                               </div>
+                              {checked && (
+                                <div className="shrink-0 text-white">
+                                  <CheckIcon className="h-6 w-6"/>
+                                </div>
+                              )}
                             </div>
-                            {checked && (
-                              <div className="shrink-0 text-white">
-                                <CheckIcon className="h-6 w-6"/>
-                              </div>
-                            )}
-                          </div>
-                        </>
-                      )}
-                    </RadioGroup.Option>
-                  </div>
-                </RadioGroup>
-              </div>)}
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    </div>
+                  </RadioGroup>
+                </div>)}
 
-            <div className="col-span-6">
-              <button type="button"
-                      onClick={handleOnSubmit}
-                      className="inline-flex justify-center mr-3 rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-indigo-300 focus-visible:ring-offset-2">
-                Agregar producto
-              </button>
+              <div className="col-span-6 pt-5">
+                <button type="submit"
+                        className="inline-flex w-full justify-center rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 disabled:bg-gray-200 disabled:text-gray-400">
+                  Agregar producto
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
--- components/sales/form/index.tsx
+++ components/sales/form/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC, Fragment, useEffect, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { useSupabaseClient } from '@supabase/auth-helpers-react'
 import { useMutation } from '@tanstack/react-query'
@@ -16,13 +16,14 @@ import {
   TableHeaderCell,
   TableRow
 } from '@tremor/react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/20/solid'
 
 interface SalesInputs {
   id?: number
   customer_id: string
   employee_id: string
   total: number
-
 }
 
 const RegisterSaleForm: FC = () => {
@@ -65,8 +66,6 @@ const RegisterSaleForm: FC = () => {
     }
   })
 
-  const [detailsAdded, setDetailsAdded] = useState<SaleDetailInput[]>([])
-
   const onSubmit: SubmitHandler<SalesInputs> = (data: SalesInputs) => {
     mutateSale(data)
   }
@@ -87,6 +86,18 @@ const RegisterSaleForm: FC = () => {
     }
   }, [sale])
 
+  const [detailsAdded, setDetailsAdded] = useState<SaleDetailInput[]>([])
+  useEffect(() => {
+    setValue('total', detailsAdded
+      .map(item => Number(item.price) * Number(item.quantity))
+      .reduce((accumulator, currentValue) =>
+        accumulator + currentValue, 0))
+  }, [detailsAdded])
+
+  const [addDetailFormOpen, setAddDetailFormOpen] = useState(false)
+  const openAddDetailForm = (): void => setAddDetailFormOpen(true)
+  const closeAddDetailForm = (): void => setAddDetailFormOpen(false)
+
   const handleAddDetail = (detail: SaleDetailInput): void => {
     const exists = detailsAdded.find(d => d.product?.code === detail.product?.code && d.price === Number(detail.price))
     if (exists === undefined) {
@@ -100,14 +111,9 @@ const RegisterSaleForm: FC = () => {
       })
       setDetailsAdded(newDetails)
     }
-  }
 
-  useEffect(() => {
-    setValue('total', detailsAdded
-      .map(item => Number(item.price) * Number(item.quantity))
-      .reduce((accumulator, currentValue) =>
-        accumulator + currentValue, 0))
-  }, [detailsAdded])
+    closeAddDetailForm()
+  }
 
   return (
     <div className="mt-5">
@@ -123,13 +129,77 @@ const RegisterSaleForm: FC = () => {
               </div>
 
               <div className="col-span-6">
-                <SaleDetailForm
-                  onSubmit={handleAddDetail}/>
+                <button onClick={openAddDetailForm}
+                        className="inline-flex justify-center rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 disabled:bg-gray-200 disabled:text-gray-400">
+                  Agregar producto
+                </button>
+
+                <Transition appear show={addDetailFormOpen} as={Fragment}>
+                  <Dialog onClose={closeAddDetailForm} as="div"
+                          className="relative z-10">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-out duration-300"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in duration-200"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="fixed inset-0 bg-black bg-opacity-25"/>
+                    </Transition.Child>
+
+                    <div className="fixed inset-0 overflow-y-auto">
+                      <div
+                        className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Transition.Child
+                          as={Fragment}
+                          enter="ease-out duration-300"
+                          enterFrom="opacity-0 scale-95"
+                          enterTo="opacity-100 scale-100"
+                          leave="ease-in duration-200"
+                          leaveFrom="opacity-100 scale-100"
+                          leaveTo="opacity-0 scale-95"
+                        >
+                          <Dialog.Panel
+                            className="w-full max-w-xl transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                            <Dialog.Title
+                              as="h3"
+                              className="text-lg font-medium leading-6 text-gray-900"
+                            >
+                              <div className="flex flex-col mb-5">
+                                <div className="flex flex-row justify-between">
+                                  <h3
+                                    className="text-xl font-semibold text-gray-900">
+                                    Agregar producto
+                                  </h3>
+                                  <button
+                                    onClick={closeAddDetailForm}
+                                    className="inline-flex justify-center rounded-full border border-transparent bg-white px-4 py-2 text-sm font-medium text-red-700 hover:bg-red-50 focus:outline-none">
+                                    <XMarkIcon
+                                      className="h-5 w-5 text-red-700"
+                                    />
+                                  </button>
+                                </div>
+                              </div>
+                            </Dialog.Title>
+
+                            <div className="mt-2">
+                              <SaleDetailForm
+                                onSubmit={handleAddDetail}/>
+                            </div>
+                          </Dialog.Panel>
+                        </Transition.Child>
+                      </div>
+                    </div>
+                  </Dialog>
+                </Transition>
               </div>
 
               <div className="col-span-6">
                 <div className="overflow-x-auto relative">
                   <Card>
+                    <h3 className="text-xl">Productos añadidos</h3>
                     <Table>
                       <TableHead>
                         <TableRow>
