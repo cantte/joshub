b83bd113316c813dfc9ce97b3c51745227ddb0bf
Carlos Pavajeau <cantte098@gmail.com>
2022-12-11 16:03:15 -0500

    feat(products): move Supabase api calls to Next.js api routes

---
+++ pages/api/products/[code]/index.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  if (req.method === 'DELETE') {
+    const supabase = createServerSupabaseClient({ req, res })
+
+    const { code } = req.query
+    const { data, error } = await supabase.from('products')
+      .update({ deleted_at: new Date() }).eq('code', code as string)
+
+    if (error != null) {
+      res.status(500).json({ error: error.message })
+      return
+    }
+
+    res.status(200).json(data)
+    return
+  }
+
+  if (req.method === 'PUT') {
+    const supabase = createServerSupabaseClient({ req, res })
+
+    const { code } = req.query
+    const { body } = req
+    const { data, error } = await supabase.from('products')
+      .update({
+        ...body,
+        updated_at: new Date()
+      }).eq('code', code as string)
+
+    if (error != null) {
+      res.status(500).json({ error: error.message })
+      return
+    }
+
+    res.status(200).json(data)
+    return
+  }
+
+  res.status(405).json({ error: 'Method not allowed' })
+}
+
+export default handler
---
+++ pages/api/products/index.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+
+const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  if (req.method === 'GET') {
+    const supabase = createServerSupabaseClient({ req, res })
+
+    const { data, error } = await supabase.from('products')
+      .select().is('deleted_at', null)
+
+    if (error != null) {
+      res.status(500).json({ error: error.message })
+      return
+    }
+
+    res.status(200).json(data)
+    return
+  }
+
+  if (req.method === 'POST') {
+    const supabase = createServerSupabaseClient({ req, res })
+
+    const { body } = req
+    const { data, error } = await supabase.from('products').insert(body).select()
+
+    if (error != null) {
+      res.status(500).json({ error: error.message })
+      return
+    }
+
+    res.status(200).json(data)
+    return
+  }
+
+  res.status(405).json({ error: 'Method not allowed' })
+}
+
+export default handler
