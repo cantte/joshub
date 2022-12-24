01077d1cd4606d54a6698e5c5652131a1c104d16
Carlos Andrés Pavajeau Max <31832701+cantte@users.noreply.github.com>
2022-12-24 15:44:24 -0500

    feat/new navbar (#16)
    * feat: initial version of new navbar
    
    * feat: finish new navbar
    
    completing JOS-9

--- components/shared/navbar/index.tsx
+++ components/shared/navbar/index.tsx
@@ -1,9 +1,49 @@
-import React, { FC } from 'react'
-import { Disclosure } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import React, { FC, Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import {
+  Bars3Icon,
+  ChevronDownIcon,
+  QrCodeIcon,
+  UserGroupIcon,
+  XMarkIcon
+} from '@heroicons/react/24/outline'
 import NextLink from 'next/link'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
 import { useRouter } from 'next/router'
+
+const navigation = [
+  { name: 'Registrar venta', href: '/sales/register' },
+  { name: 'Registrar domicilio', href: '/orders/register' },
+  { name: 'Registrar cliente', href: '/customers/register' }
+]
+
+const storeNavigation = [
+  {
+    name: 'Empleados',
+    href: '/employees',
+    description: 'Administrar empleados',
+    icon: UserGroupIcon
+  },
+  {
+    name: 'Productos',
+    href: '/products',
+    description: 'Administrar productos',
+    icon: QrCodeIcon
+  }
+]
+
+const mobileNavigation = [
+  { name: 'Registrar venta', href: '/sales/register' },
+  { name: 'Registrar domicilio', href: '/orders/register' },
+  { name: 'Registrar cliente', href: '/customers/register' },
+  { name: 'Empleados', href: '/employees' },
+  { name: 'Productos', href: '/products' }
+]
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+function classNames (...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const Navbar: FC = () => {
   const session = useSession()
@@ -16,95 +56,199 @@ const Navbar: FC = () => {
   }
 
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div
-                className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button
-                  className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open
-                    ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true"/>
-                      )
-                    : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true"/>
-                      )}
-                </Disclosure.Button>
-              </div>
-              <div
-                className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <span className="text-white text-xl">Joshub</span>
+    <Popover className='relative bg-white'>
+      <div className='mx-auto max-w-7xl px-4 sm:px-6'>
+        <div
+          className='flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10'>
+          <div className='flex justify-start lg:w-0 lg:flex-1'>
+            <NextLink href='/' className='-m-1.5 p-1.5'>
+              <span className='text-2xl'>Joshub</span>
+            </NextLink>
+          </div>
+
+          <div className='-my-2 -mr-2 md:hidden'>
+            <Popover.Button
+              className='inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none'>
+              <span className='sr-only'>Open menu</span>
+              <Bars3Icon className='h-6 w-6' aria-hidden='true' />
+            </Popover.Button>
+          </div>
+
+          <Popover.Group as='nav' className='hidden space-x-10 md:flex'>
+            {
+              (session != null) && (
+                <>
+                  <Popover className='relative'>
+                    {({ open }) => (
+                      <>
+                        <Popover.Button
+                          className={classNames(
+                            open ? 'text-gray-900' : 'text-gray-500',
+                            'group inline-flex items-center rounded-md bg-white text-base hover:text-gray-900 focus:outline-none'
+                          )}
+                        >
+                          <span>Tienda</span>
+                          <ChevronDownIcon
+                            className={classNames(
+                              open ? 'text-gray-600' : 'text-gray-400',
+                              'ml-2 h-5 w-5 group-hover:text-gray-500'
+                            )}
+                            aria-hidden='true'
+                          />
+                        </Popover.Button>
+
+                        <Transition
+                          as={Fragment}
+                          enter='transition ease-out duration-200'
+                          enterFrom='opacity-0 translate-y-1'
+                          enterTo='opacity-100 translate-y-0'
+                          leave='transition ease-in duration-150'
+                          leaveFrom='opacity-100 translate-y-0'
+                          leaveTo='opacity-0 translate-y-1'
+                        >
+                          <Popover.Panel
+                            className='absolute z-10 -ml-4 mt-3 w-screen max-w-md transform px-2 sm:px-0 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2'>
+                            <div
+                              className='overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5'>
+                              <div
+                                className='relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8'>
+                                {storeNavigation.map((item) => (
+                                  <NextLink
+                                    key={item.name}
+                                    href={item.href}
+                                    className='-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50'
+                                  >
+                                    <item.icon
+                                      className='h-6 w-6 flex-shrink-0 text-indigo-700'
+                                      aria-hidden='true' />
+                                    <div className='ml-4'>
+                                      <p
+                                        className='text-base text-gray-900'>{item.name}</p>
+                                      <p
+                                        className='mt-1 text-sm text-gray-500'>{item.description}</p>
+                                    </div>
+                                  </NextLink>
+                                ))}
+                              </div>
+                            </div>
+                          </Popover.Panel>
+                        </Transition>
+
+                      </>
+                    )}
+                  </Popover>
+
+                  <div
+                    className='hidden lg:flex lg:min-w-0 lg:justify-center space-x-10'>
+                    {navigation.map((item) => (
+                      <NextLink key={item.name} href={item.href}
+                                className='text-gray-500 inline-flex items-center rounded-md bg-white text-base hover:text-gray-900'>
+                        {item.name}
+                      </NextLink>
+                    ))}
+                  </div>
+                </>
+              )
+            }
+          </Popover.Group>
+
+          <div
+            className='hidden items-center justify-end md:flex md:flex-1 lg:w-0'>
+            {
+              session === null && (
+                <NextLink
+                  href='/login'
+                  className='inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20'
+                >
+                  Iniciar sesión
+                </NextLink>
+              )
+            }
+
+            {
+              session !== null && (
+                <button
+                  type='button'
+                  onClick={handleLogout}
+                  className='inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20'
+                >
+                  Cerrar sesión
+                </button>
+              )
+            }
+          </div>
+        </div>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter='duration-200 ease-out'
+        enterFrom='opacity-0 scale-95'
+        enterTo='opacity-100 scale-100'
+        leave='duration-100 ease-in'
+        leaveFrom='opacity-100 scale-100'
+        leaveTo='opacity-0 scale-95'>
+        <Popover.Panel focus
+                       className='absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden z-50'>
+          <div
+            className='divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5'>
+            <div className='px-5 pt-5 pb-6'>
+              <div className='flex items-center justify-between'>
+                <div>
+                  <span className='text-2xl'>Joshub</span>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  {session !== null
-                    ? <div className="flex space-x-4">
-                      <NextLink
-                        href="/"
-                        className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">
-                        Panel de control
-                      </NextLink>
-
-                      <NextLink
-                        href="/products"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                        Productos
-                      </NextLink>
-
-                      <NextLink
-                        href="/employees"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                        Empleados
-                      </NextLink>
-
-                      <NextLink
-                        href="/customers/register"
-                        className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
-                        Registrar cliente
-                      </NextLink>
-                    </div>
-                    : null}
+                <div className='-mr-2'>
+                  <Popover.Button
+                    className='inline-flex items-center justify-center rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none'>
+                    <span className='sr-only'>Close menu</span>
+                    <XMarkIcon className='h-6 w-6' aria-hidden='true' />
+                  </Popover.Button>
                 </div>
               </div>
-              <div
-                className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 hidden sm:ml-6 sm:block">
+              <div className='mt-6'>
+                <nav className='grid gap-y-8'>
+                  {mobileNavigation.map((item) => (
+                    <NextLink key={item.name} href={item.href}
+                              className='-m-3 p-3 flex items-center rounded-lg hover:bg-gray-50'>
+                      <div className='ml-4 text-base font-medium text-gray-900'>
+                        {item.name}
+                      </div>
+                    </NextLink>
+                  ))}
+                </nav>
+              </div>
+            </div>
+
+            <div className='space-y-6 py-6 px-5'>
+              <div className='grid grid-cols-2 gap-y-4 gap-x-8'>
                 {
-                  session === null
-                    ? (
-                      <NextLink href="/login">
-                        <button
-                          className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 mx-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                          Iniciar sesión
-                        </button>
-                      </NextLink>
-                      )
-                    : (
-                      <button
-                        onClick={handleLogout}
-                        className="inline-flex justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus-visible:ring-1 focus-visible:ring-red-500 focus-visible:ring-offset-2">
-                        Cerrar sesión
-                      </button>
-                      )
+                  session === null && (
+                    <NextLink
+                      href='/login'
+                      className='text-base font-medium text-gray-900 hover:text-gray-700'
+                    >
+                      Iniciar sesión
+                    </NextLink>
+                  )
+                }
+
+                {
+                  session !== null && (
+                    <button
+                      type='button'
+                      onClick={handleLogout}
+                      className='text-base font-medium text-gray-900 text-left hover:text-gray-700'
+                    >
+                      Cerrar sesión
+                    </button>
+                  )
                 }
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-              <Disclosure.Button
-                className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
-                Dashboard
-              </Disclosure.Button>
-            </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
+        </Popover.Panel>
+      </Transition>
+    </Popover>
   )
 }
 
