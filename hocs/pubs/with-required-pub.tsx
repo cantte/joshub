b3d2b0b8e60f3b6792fc4b36719fd9a6e7fdb993
Carlos Pavajeau <cantte098@gmail.com>
2022-12-25 13:33:24 -0500

    feat(pubs): add pub verification
    the requirement to pre-register a pub is added to the pages.
    
    completing JOS-4, JOS-8

---
+++ hocs/pubs/with-required-pub.tsx
@@ -0,0 +1,49 @@
+import { ComponentType, FC, ReactElement, useEffect } from 'react'
+import { usePubsStore } from '@joshub/store/pubs'
+import { useUser } from '@supabase/auth-helpers-react'
+import { Pub } from '@joshub/types/pubs'
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+import { useRouter } from 'next/router'
+
+const withRequiredPub = (Component: ComponentType): FC => {
+  const WithRequiredPub = (): ReactElement => {
+    const { setPub } = usePubsStore()
+
+    const user = useUser()
+
+    const fetchPub = async (): Promise<Pub> => {
+      const { data } = await axios.get(`/api/pubs?owner=${user?.id ?? ''}`)
+
+      return data
+    }
+
+    const { data: pub } = useQuery(
+      ['pubs', user?.id],
+      fetchPub,
+      {
+        enabled: user !== undefined && user !== null,
+        retry: false,
+        refetchOnWindowFocus: false
+      }
+    )
+
+    const router = useRouter()
+    useEffect(() => {
+      if (pub !== undefined && pub !== null) {
+        setPub(pub)
+        return
+      }
+
+      void router.push('/pubs/register')
+    }, [pub])
+
+    return <>
+      {(pub !== undefined && pub !== null) && <Component />}
+    </>
+  }
+
+  return WithRequiredPub
+}
+
+export default withRequiredPub
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,28 +9,29 @@ import NextLink from 'next/link'
 import SalesTable from '@components/sales/table'
 import OrdersTable from '@components/orders/table'
 import { withRequiredAuth } from '@joshub/shared/auth/with-required-auth'
+import withRequiredPub from '@joshub/hocs/pubs/with-required-pub'
 
 const Home: FC = () => {
   return (
     <DefaultLayout>
-      <div className="flex flex-col mb-10">
-        <div className="flex flex-row justify-between">
-          <h1 className="text-4xl text-gray-900">
+      <div className='flex flex-col mb-10'>
+        <div className='flex flex-row justify-between'>
+          <h1 className='text-4xl text-gray-900'>
             Bienvenido
           </h1>
 
           <div>
-            <NextLink href="/sales/register">
+            <NextLink href='/sales/register'>
               <button
-                className="inline-flex justify-center mr-3 rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-indigo-300 focus-visible:ring-offset-2">
-                <PlusIcon className="-ml-1 mr-2 h-5 w-5 text-indigo-900"/>
+                className='inline-flex justify-center mr-3 rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-indigo-300 focus-visible:ring-offset-2'>
+                <PlusIcon className='-ml-1 mr-2 h-5 w-5 text-indigo-900' />
                 Registrar venta
               </button>
             </NextLink>
-            <NextLink href="/orders/register">
+            <NextLink href='/orders/register'>
               <button
-                className="inline-flex justify-center rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-indigo-300 focus-visible:ring-offset-2">
-                <PlusIcon className="-ml-1 mr-2 h-5 w-5 text-indigo-900"/>
+                className='inline-flex justify-center rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-indigo-300 focus-visible:ring-offset-2'>
+                <PlusIcon className='-ml-1 mr-2 h-5 w-5 text-indigo-900' />
                 Registrar domicilio
               </button>
             </NextLink>
@@ -38,35 +39,35 @@ const Home: FC = () => {
         </div>
       </div>
 
-      <h2 className="text-2xl text-gray-700">
+      <h2 className='text-2xl text-gray-700'>
         Reporte diario
       </h2>
 
-      <div className="sm:rounded-md">
-        <div className="mb-7">
-          <div className="mt-4">
-            <ColGrid numColsSm={2} numColsLg={3} gapX="gap-x-6" gapY="gap-y-6">
-              <SalesCard/>
-              <ProfitsCard/>
-              <TotalProductsCard/>
+      <div className='sm:rounded-md'>
+        <div className='mb-7'>
+          <div className='mt-4'>
+            <ColGrid numColsSm={2} numColsLg={3} gapX='gap-x-6' gapY='gap-y-6'>
+              <SalesCard />
+              <ProfitsCard />
+              <TotalProductsCard />
             </ColGrid>
           </div>
         </div>
       </div>
 
-      <h2 className="text-2xl text-gray-700">
+      <h2 className='text-2xl text-gray-700'>
         Últimas ventas
       </h2>
-      <SalesTable/>
+      <SalesTable />
 
-      <h2 className="text-2xl text-gray-700 mt-7">
+      <h2 className='text-2xl text-gray-700 mt-7'>
         Últimos domicilios
       </h2>
-      <OrdersTable/>
+      <OrdersTable />
     </DefaultLayout>
   )
 }
 
 export const getServerSideProps = withRequiredAuth
 
-export default Home
+export default withRequiredPub(Home)
