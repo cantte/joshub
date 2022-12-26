d09e00e3b2fc806ddbeac9bee7af9b17e655dd89
Carlos Pavajeau <cantte098@gmail.com>
2022-12-25 20:14:59 -0500

    feat(reports): linking to a pub
    completing JOS-4

--- pages/api/reports/today/index.ts
+++ pages/api/reports/today/index.ts
@@ -4,12 +4,14 @@ import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
 const handler = async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   if (req.method === 'GET') {
     const supabase = createServerSupabaseClient({ req, res })
+    const { pubId } = req.query
 
     const {
       data,
       error
     } = await supabase.from('daily_reports').select()
       .eq('created_at', new Date().toISOString().split('T')[0])
+      .eq('pub_id', pubId)
 
     if (error !== null) {
       res.status(500).json({ error: error.message })
