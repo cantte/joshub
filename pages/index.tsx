20e892019a6c18cc001757cab3fe6ac673ba6bea
Carlos Pavajeau <cantte098@gmail.com>
2022-12-25 20:37:19 -0500

    refactor: remove unnecessary buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import { FC } from 'react'
 import DefaultLayout from '@components/shared/layout/default'
 import { ColGrid } from '@tremor/react'
-import { PlusIcon } from '@heroicons/react/20/solid'
 import SalesCard from '@components/dashboard/sales'
 import TotalProductsCard from '@components/dashboard/products'
 import ProfitsCard from '@components/dashboard/profits'
-import NextLink from 'next/link'
 import SalesTable from '@components/sales/table'
 import OrdersTable from '@components/orders/table'
 import { withRequiredAuth } from '@joshub/shared/auth/with-required-auth'
@@ -17,21 +15,6 @@ const Home: FC = () => {
       <div className='flex flex-col mb-10'>
         <div className='flex flex-row justify-between'>
           <h1 className='text-4xl text-gray-900'>Bienvenido</h1>
-
-          <div>
-            <NextLink href='/sales/register'>
-              <button className='inline-flex justify-center mr-3 rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-indigo-300 focus-visible:ring-offset-2'>
-                <PlusIcon className='-ml-1 mr-2 h-5 w-5 text-indigo-900' />
-                Registrar venta
-              </button>
-            </NextLink>
-            <NextLink href='/orders/register'>
-              <button className='inline-flex justify-center rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-indigo-300 focus-visible:ring-offset-2'>
-                <PlusIcon className='-ml-1 mr-2 h-5 w-5 text-indigo-900' />
-                Registrar domicilio
-              </button>
-            </NextLink>
-          </div>
         </div>
       </div>
 
