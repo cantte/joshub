99fdb8ad92197d28f965b5dc9bda06009c00a543
cantte <cantte098@gmail.com>
2022-12-29 15:11:26 -0500

    feat(customers): add new register customer ui
    completed JOS-13

--- components/customers/register/index.tsx
+++ components/customers/register/index.tsx
@@ -5,23 +5,38 @@ import axios from 'axios'
 import { CustomerInputs } from '@joshub/types/customers'
 import toast from 'react-hot-toast'
 import Alert from '@components/shared/feedback/alerts'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const CustomerSchema = z.object({
+  id: z.string().min(1, 'El id es requerido'),
+  name: z.string().min(1, 'El nombre es requerido')
+})
 
 const RegisterCustomerForm: FC = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: {
+      errors,
+      isSubmitting
+    },
     reset
-  } = useForm<CustomerInputs>()
+  } = useForm<CustomerInputs>({
+    resolver: zodResolver(CustomerSchema)
+  })
 
   const saveCustomer = async (data: CustomerInputs): Promise<void> => {
     await axios.post('/api/customers', data)
   }
 
-  const { mutate, isLoading, error } = useMutation(saveCustomer, {
+  const {
+    mutate,
+    isLoading
+  } = useMutation(saveCustomer, {
     onSuccess: () => {
       toast.custom(t => (
-        <Alert id={t.id} title='Cliente registrado!' variant='success' />
+        <Alert id={t.id} title="Cliente registrado!" variant="success"/>
       ))
       reset()
     }
@@ -32,76 +47,49 @@ const RegisterCustomerForm: FC = () => {
   }
 
   return (
-    <div className='mt-5'>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className='sm:shadow sm:rounded-md'>
-          <div className='bg-white sm:px-4 py-5 sm:p-6'>
-            <div className='grid grid-cols-6 gap-6'>
-              <div className='col-span-6 sm:col-span-3'>
-                <label
-                  htmlFor='id'
-                  className='block text-sm font-medium text-gray-700'
-                >
-                  Identificación
-                </label>
-                <input
-                  type='text'
-                  className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                  id='id'
-                  {...register('id', { required: true })}
-                />
-
-                {errors.name != null && (
-                  <span className='text-red-400 text-xs block py-1'>
-                    Este campo es requerido
-                  </span>
-                )}
-              </div>
-
-              <div className='col-span-6 sm:col-span-3'>
-                <label
-                  htmlFor='name'
-                  className='block text-sm font-medium text-gray-700'
-                >
-                  Nombre
-                </label>
-                <input
-                  type='text'
-                  className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm'
-                  id='name'
-                  {...register('name', { required: true })}
-                />
-                {errors.name != null && (
-                  <span className='text-red-400 text-xs block py-1'>
-                    Este campo es requerido
-                  </span>
-                )}
-              </div>
-
-              {Boolean(error) && (
-                <div
-                  className='p-4 w-full col-span-6 mt-3 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800'
-                  role='alert'
-                >
-                  Ya existe un cliente registrado con la identificación
-                  ingresada.
-                </div>
-              )}
-
-              <div className='py-3 col-span-6'>
-                <button
-                  type='submit'
-                  disabled={isLoading}
-                  className='inline-flex w-full justify-center rounded-full border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-500 focus-visible:ring-offset-2 disabled:bg-gray-200 disabled:text-gray-400'
-                >
-                  Guardar
-                </button>
-              </div>
-            </div>
-          </div>
+    <form className="space-y-10" onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <div>
+          <label className="block">
+            <span className="block">Id</span>
+            <input
+              type="text"
+              className="block border text-lg px-4 py-3 mt-2 rounded-lg border-gray-200 focus:bg-white text-gray-900 focus:border-blue-600 focus:ring-0 outline-none w-full  disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed"
+              {...register('id')}
+              disabled={isLoading || isSubmitting}
+            />
+          </label>
+          {errors.id != null && (
+            <p className="text-sm text-red-600 mt-1">{errors.id.message}</p>
+          )}
         </div>
-      </form>
-    </div>
+
+        <div className="mt-6">
+          <label className="block">
+            <span className="block">Nombre</span>
+            <input
+              type="text"
+              className="block border text-lg px-4 py-3 mt-2 rounded-lg border-gray-200 focus:bg-white text-gray-900 focus:border-blue-600 focus:ring-0 outline-none w-full  disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed"
+              {...register('name')}
+              disabled={isLoading || isSubmitting}
+            />
+          </label>
+          {errors.name != null && (
+            <p className="text-sm text-red-600 mt-1">{errors.name.message}</p>
+          )}
+        </div>
+
+        <div className="mt-8">
+          <button
+            type="submit"
+            className="text-base w-full px-6 py-3.5 font-medium text-center text-indigo-900 bg-indigo-100 rounded-full hover:bg-indigo-200 border border-transparent disabled:bg-gray-100 disabled:text-gray-400"
+            disabled={isSubmitting || isLoading}
+          >
+            Guardar
+          </button>
+        </div>
+      </div>
+    </form>
   )
 }
 
