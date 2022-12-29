5cab5b2fe5dc34267676d6ce031c3a7cee8d87a0
cantte <cantte098@gmail.com>
2022-12-29 11:57:08 -0500

    feat(pubs): register pub owner when register a pub

--- types/pubs/index.ts
+++ types/pubs/index.ts
@@ -1,8 +1,14 @@
+import { Employee } from '@joshub/types/employees'
+
+type PubOwnerInputs = Omit<Employee, 'user_id' | 'pub_id'>
+
 export interface PubInputs {
   name: string
   nit?: string
   address: string
-  owner: string
+  owner: PubOwnerInputs
+
+  user_id: string
 }
 
 export type Pub = PubInputs & { id: string, created_at: string }
