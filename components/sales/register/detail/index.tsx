5b9d7ee05b2bedd35658aff2a932e5309b2de43a
cantte <cantte098@gmail.com>
2022-12-30 14:53:29 -0500

    fix: include product code on zod schema

--- components/sales/register/detail/index.tsx
+++ components/sales/register/detail/index.tsx
@@ -10,6 +10,7 @@ import { z } from 'zod'
 const SaleDetailSchema = z
   .object({
     product: z.object({
+      code: z.string(),
       quantity: z.coerce.number().optional()
     }),
     quantity: z.coerce
