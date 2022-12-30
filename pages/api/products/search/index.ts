9a5fc3be45e5ec58c9c62a0e909f673eb587c622
cantte <cantte098@gmail.com>
2022-12-30 13:04:40 -0500

    feat(products): add api endpoint for search products by name

---
+++ pages/api/products/search/index.ts
@@ -0,0 +1,36 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<void> => {
+  if (req.method === 'GET') {
+    const supabase = createServerSupabaseClient({
+      req,
+      res
+    })
+
+    const { pubId, search } = req.query
+    const { data, error } = await supabase
+      .from('products')
+      .select()
+      .is('deleted_at', null)
+      .eq('pub_id', pubId as string)
+      .ilike('name', `%${search as string}%`)
+      .order('name', { ascending: true })
+      .limit(3)
+
+    if (error != null) {
+      res.status(500).json({ error: error.message })
+      return
+    }
+
+    res.status(200).json(data)
+    return
+  }
+
+  res.status(405).json({ error: 'Method not allowed' })
+}
+
+export default handler
